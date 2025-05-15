64f578df397bc877e7c0d0c68a49131db7039922
Deku0019523f <alecbitty@gmail.com>
2025-05-15 08:33:11 +0000

    Add files via upload

---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+  // FREE
+  { title: "Cours WiFi", price: 0, category: "FREE", img: "images/cours-wifi.jpg" },
+  { title: "Bot WhatsApp", price: 0, category: "FREE", img: "images/bot-whatsapp.jpg" },
+  { title: "Script Host Fonctionnel", price: 0, category: "FREE", img: "images/script-host-fonctionnel.jpg" },
+
+  // Abonnement
+  { title: "Internet Orange CI - 80 Go", price: 8000, category: "Abonnement", img: "images/internet-orange-ci-80-go.jpg" },
+  { title: "Internet Orange CI - 100 Go", price: 13000, category: "Abonnement", img: "images/internet-orange-ci-100-go.jpg" },
+  { title: "Internet Orange CI - 48 Go", price: 6500, category: "Abonnement", img: "images/internet-orange-ci-48-go.jpg" },
+  { title: "BOOSTAGE TIKTOK - 1000 abonnés", price: 1500, category: "Abonnement", img: "images/boostage-tiktok.jpg" },
+  { title: "BOOSTAGE YOUTUBE - 1000 abonnés", price: 5000, category: "Abonnement", img: "images/boostage-youtube.jpg" },
+  { title: "BOOSTAGE INSTAGRAM - 1000 abonnés", price: 1000, category: "Abonnement", img: "images/boostage-instagram.jpg" },
+  { title: "Netflix - 1 mois", price: 2400, category: "Abonnement", img: "images/netflix-1-mois.jpg" },
+  { title: "Netflix - 3 mois", price: 7000, category: "Abonnement", img: "images/netflix-3-mois.jpg" },
+  { title: "VPS 2GB RAM", price: 2500, category: "Abonnement", img: "images/vps-2gb-ram.jpg" },
+  { title: "VPS 4GB RAM", price: 4000, category: "Abonnement", img: "images/vps-4gb-ram.jpg" },
+
+  // Logiciel
+  { title: "Logiciel d’espionnage", price: 3000, category: "Logiciel", img: "images/logiciel-espionnage.jpg" },
+  { title: "Script Chumogh SSH #SLOWDNS", price: 5000, category: "Logiciel", img: "images/script-chumogh-ssh.jpg" },
+  { title: "Script + VPS pour WhatsApp Bot", price: 3000, category: "Logiciel", img: "images/script-vps-whatsapp.jpg" },
+  { title: "Hack caméra frontale avec Termux", price: 2000, category: "Logiciel", img: "images/hack-camera-termux.jpg" },
+  { title: "Outils de Phishing avec Termux", price: 3000, category: "Logiciel", img: "images/outils-phishing-termux.jpg" }
+];
